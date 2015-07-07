bf4ce74a20b866b469e16beece4e6d220b9af9bf
Steven Ng <steven.ng@braintapper.com>
2015-07-07 17:16:44 -0400

    Prewire both mssql and postgresql

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-var callback, config, options, postgresql, sql;
+var callback, config, cson, databaseConfig, fs, logger, mssql, options, postgresql, sql, writeDataFile;
 
 postgresql = require("./lib/postgresql").postgresql;
 
-sql = "select * from users";
+mssql = require("./lib/mssql").mssql;
+
+cson = require("cson");
+
+fs = require("fs");
+
+logger = require('knodeo-logger').Logger;
 
 config = require('knodeo-configuration').Configuration;
 
 config.load('config.sql.cson');
 
-options = config.current.databases.knodeo;
+writeDataFile = function(data, path) {
+  var output;
+  output = cson.createString(data);
+  return fs.writeFileSync(path, output, "UTF-8");
+};
 
-options.database = "knodeo";
+sql = "select * from users";
 
-callback = function(results) {
-  return console.log(results.rows);
+options = config.current.databases.pi_dev;
+
+options.database = "pi_dev";
+
+callback = function(data) {
+  return writeDataFile(data, "postgresql.cson");
 };
 
 postgresql.execute(sql, options, callback);
+
+databaseConfig = config.current.databases['test'];
+
+options = {
+  userName: databaseConfig.user,
+  password: databaseConfig.password,
+  server: databaseConfig.host,
+  options: {
+    port: databaseConfig.port,
+    database: 'test',
+    rowCollectionOnDone: true
+  }
+};
+
+callback = function(data) {
+  return writeDataFile(data, "mssql.cson");
+};
+
+mssql.execute("select * from names", options, callback);
--- src/index.coffee
+++ src/index.coffee
@@ -1,15 +1,45 @@
 # index.coffee
 
 postgresql = require("./lib/postgresql").postgresql
+mssql = require("./lib/mssql").mssql
+cson = require "cson"
+fs = require "fs"
+logger = require('knodeo-logger').Logger
+config = require('knodeo-configuration').Configuration
+config.load 'config.sql.cson'
+
+
+writeDataFile = (data, path)->
+  output = cson.createString(data)
+  fs.writeFileSync path, output, "UTF-8"
+
 
 sql = "select * from users"
 
-config = require('knodeo-configuration').Configuration
-config.load 'config.sql.cson'
-options = config.current.databases.knodeo
-options.database = "knodeo"
 
-callback = (results)->
-  console.log results.rows
+options = config.current.databases.pi_dev
+options.database = "pi_dev"
+
+callback = (data)->
+  writeDataFile data, "postgresql.cson"
 
 postgresql.execute sql, options, callback
+
+
+databaseConfig = config.current.databases['test']
+
+
+# to do - move the options mapping into the mssql lib
+options = 
+  userName: databaseConfig.user
+  password: databaseConfig.password
+  server: databaseConfig.host
+  options:
+    port: databaseConfig.port
+    database: 'test'
+    rowCollectionOnDone: true
+
+callback = (data)->
+  writeDataFile data, "mssql.cson"
+
+mssql.execute "select * from names", options, callback
